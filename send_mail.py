2d8576d19d1a1592843aa8180ee56c01641e1e72
alabrashjr <alabrash.abd@gmail.com>
2019-12-30 14:52:37 +0300

    add send_mail script

---
+++ send_mail.py
@@ -0,0 +1,25 @@
+#Turn Access for less secure apps setting on
+
+import smtplib 
+def send_email(recipient, subject, body): 
+     user,pwd=(<USERNAME>,<PASSWORD>)
+     FROM = user 
+     TO = recipient if isinstance(recipient, list) else [recipient] 
+     SUBJECT = subject 
+     TEXT = body 
+  
+     # Prepare actual message 
+     message = """From: %s\nTo: %s\nSubject: %s\n\n%s 
+     """ % (FROM, ", ".join(TO), SUBJECT, TEXT) 
+     try: 
+         server = smtplib.SMTP("smtp.gmail.com", 587) 
+         server.ehlo() 
+         server.starttls() 
+         server.login(user, pwd) 
+         server.sendmail(FROM, TO, message) 
+         server.close() 
+         print('successfully sent the mail') 
+         
+     except Exception as e: 
+         print("failed to send mail") 
+         print(e.with_traceback()) 
